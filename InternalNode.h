c57f4f665d7c41cda3cf5d36cadf3fedb93375b7
Soronie <rafaeliarroyo@hotmail.com>
2017-01-09 20:26:23 -0800

    Add files via upload

---
+++ InternalNode.h
@@ -0,0 +1,30 @@
+#fndef InternalNodeH
+  #define InternalNodeH
+
+#include "BTreeNode.h"
+
+class InternalNode:public BTreeNode
+{
+  int internalSize;
+  BTreeNode **children;
+  int *keys;
+public:
+  InternalNode(int ISize, int LSize, InternalNode *p,
+    BTreeNode *left, BTreeNode *right);
+  int getMinimum()const;
+  int getMaximum()const;
+  InternalNode* insert(int value); // returns pointer to new InternalNode
+    // if it splits else NULL
+  void insert(BTreeNode *oldRoot, BTreeNode *node2); // if root splits use this
+  InternalNode* insert(BTreeNode *newNode); // from a sibling
+  void print(Queue <BTreeNode*> &queue);
+  void printnewLeaf();
+  void rollRightAndInsert(BTreeNode* value, int pos);
+  void rollLeft();
+  InternalNode* split(BTreeNode* value);
+  BTreeNode* insertValueInMiddle(int value);
+  void insertNodeInMiddle(BTreeNode* value);
+  void updateKeys();
+}; // InternalNode class
+
+#endif
---
+++ LeafNode.h
@@ -0,0 +1,23 @@
+#ifndef LeafNodeH
+#define LeafNodeH
+
+#include "BTreeNode.h"
+
+class LeafNode:public BTreeNode
+{
+  int *values;
+public:
+  LeafNode(int LSize, InternalNode *p, BTreeNode *left,
+    BTreeNode *right);
+  int getMinimum() const;
+  int getMaximum() const;
+  LeafNode* insert(int value); // returns pointer to new Leaf if splits
+  // else NULL
+  void print(Queue <BTreeNode*> &queue);
+  void rollRightAndInsert(int value, int pos);
+  void rollLeft();
+  LeafNode* split(int value);
+  void insertInMiddle(int value);
+}; //LeafNode class
+
+#endif
